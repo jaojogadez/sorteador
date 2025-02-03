7f02cd48d69f9c78efa660cccdcb2dc5ab25ecae
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2025-02-03 11:49:58 -0300

    começando...

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const $items = document.querySelector(".order-2");
 const $title = document.querySelector("#title");
 const $form = document.querySelector("form");
+const $result = document.getElementById("result")
 const items = `
     <div class="order-2 mt-2 mb-4">
         <div class="container-sm mb-4">
@@ -23,6 +24,18 @@ const items = `
         </div>
     </div>
   `;
+
+/* FORM's INPUTS */
+const $inputs = document.getElementsByName("numbers");
+const inputs = Array.from($inputs);
+const $totalNumbers = document.getElementById("numbers");
+const $startNumbers = document.getElementById("numbers-start");
+const $endNumbers = document.getElementById("numbers-end");
+const $btnReloadRandomNumber = document.querySelector("#result button")
+
+/* RESULTADO DO SORTEIO */
+const $resultOne = document.querySelector("#numberOne");
+const $resultTwo = document.querySelector("#numberTwo");
 
 const $switchButton = document.querySelector(".switch");
 $switchButton.onclick = () => toggleMode();
@@ -56,3 +69,45 @@ let responseWindow = () => {
 
 window.addEventListener("resize", () => responseWindow());
 window.addEventListener("DOMContentLoaded", () => responseWindow());
+
+$form.onsubmit = (event) => {
+  event.preventDefault() 
+  const totalNumbers = $totalNumbers.value
+  const startNumbers = $startNumbers.value
+  const endNumbers = $endNumbers.value
+  try {
+    const isValidInputs = inputs.every((input) => input.value != "")
+    if(isValidInputs){
+
+      $form.classList.add("d-none")
+      $result.classList.remove("d-none")
+
+      let numberOne = randomNumberInterval(startNumbers, endNumbers)
+      
+      $resultOne.innerHTML = numberOne
+      
+
+    } else{
+      alert("Por Favor, preencha todos os campos.")
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+inputs.map((input) => {
+  input.addEventListener("input", () => {
+    input.value = onlyNumbersFormat(input)
+  })
+})
+
+let onlyNumbersFormat = (input) => {
+  return input.value.replace(/\D/g, "").slice(0, 3)
+}
+
+let randomNumberInterval = (startNumber, endNumber) => {
+  startNumber = Math.ceil(startNumber)
+  endNumber = Math.floor(endNumber)
+  return Math.floor(Math.random() * (endNumber - startNumber + 1)) + startNumber
+}
